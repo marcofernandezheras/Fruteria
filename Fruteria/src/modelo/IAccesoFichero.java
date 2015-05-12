de514adac5932194ab8d780606f26828c628202e
marco <marcofernandezheras@gmail.com>
2015-05-12 15:56:45 +0200

    Interfaces Modificable e IAccesoFichero

---
+++ Fruteria/src/modelo/IAccesoFichero.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public interface IAccesoFichero {
+	/**
+	 * Leer un nuevo objeto desde su forma
+	 * serializada
+	 * @return Nuevo objeto o <code>null</code> si se llego al final del fichero.
+	 */
+	Object leerObjeto();
+	
+	/**
+	 * Serializar un objeto.
+	 * @param objeto a serializar.
+	 * @return <code>true</code> en caso de éxito, <code>false</code> en caso contrario.
+	 */
+	boolean escribirObjeto(Object objeto);
+	
+	/**
+	 * Modifica un objeto anteriormente serializado.
+	 * @param objeto Objeto a modificar.
+	 * @return <code>true</code> en caso de éxito, <code>false</code> en caso contrario.
+	 */
+	boolean modificarObjeto(Modificable objeto);
+}
---
+++ Fruteria/src/modelo/Modificable.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public interface Modificable {
+	/**
+	 * Identificador que no debe ser único para cada instancia
+	 * y no debe cambiar nunca en la instancia 
+	 * o en su representación en datos.
+	 * Ejemplo: DNI de un cliente.
+	 * @return identificador de esta instancia.
+	 */
+	Object identificador();
+}
