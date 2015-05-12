210819751241f7f1a013d8773e54b31e2f178b29
marco <marcofernandezheras@gmail.com>
2015-05-12 20:06:39 +0200

    Inicio desarrollo de modelo.AcessoFichero

---
+++ Fruteria/src/modelo/AccesoFichero.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class AccesoFichero implements IAccesoFichero {
+
+	@Override
+	public Object leerObjeto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean escribirObjeto(Object objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificarObjeto(Modificable objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
