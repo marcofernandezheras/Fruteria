05918da0af6fe3893c0ee3f18319e8c6e0bcff18
marco <marcofernandezheras@gmail.com>
2015-05-13 20:57:54 +0200

    Inicio testing

---
+++ Fruteria/src/tests/ListaArticulosTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListaArticulosTest {
+
+	@Test
+	public void testBuscarArticulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertarArticulo() {
+		fail("Not yet implemented");
+	}
+
+}
