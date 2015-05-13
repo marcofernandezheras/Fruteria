9a63c6bddabdef18489dea6dad4f4ac7eaa0622f
marco <marcofernandezheras@gmail.com>
2015-05-13 17:43:27 +0200

    Implementacion de ParaBuscarCliente

---
+++ Fruteria/src/controlador/ParaBuscarCliente.java
@@ -0,0 +1,44 @@
+package controlador;
+
+import vista.BuscarClienteUI;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import modelo.Cliente;
+
+public class ParaBuscarCliente extends BuscarClienteUI {
+	
+	private IListaCliente listaCliente;
+
+	public ParaBuscarCliente(IListaCliente listaCliente) {
+		super();
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarCliente();
+			}
+		});
+		this.listaCliente = listaCliente;
+	}
+	
+	private void buscarCliente(){
+		if(!txtBuscar.getText().isEmpty() && !txtBuscar.getText().isEmpty())//FIXME Arreglar cuando se arregle la UI
+		{
+			Cliente cliente = listaCliente.buscarCliente(txtBuscar.getText(), txtBuscar.getText());//FIXME Arreglar cuando se arregle la UI
+			if(cliente != null){
+				txtDNI.setText(cliente.getDni());
+				txtNombre.setText(cliente.getNombre());
+				txtApellido.setText(cliente.getApellidos());
+				txtColorPelo.setText(cliente.getColorPelo().toString());
+			}
+			else
+			{
+				txtMensaje.setText("No existe un cliente con ese nmbre y apellidos");
+			}
+		}
+		else
+		{
+			txtMensaje.setText("Debe introducir datos para poder buscar");
+		}
+	}
+}
