efa7ed0d8f62271557f31a5d0eac772b899464dc
maria <maria.cabeza.castillo@gmail.com>
2015-05-13 10:23:57 +0200

    Añadido constructor e implementados los metodos y sus funciones.

--- Fruteria/src/controlador/ListaArticulos.java
+++ Fruteria/src/controlador/ListaArticulos.java
@@ -1,20 +1,52 @@
 package controlador;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Vector;
 
+import modelo.AccesoFichero;
 import modelo.Articulo;
 
 public class ListaArticulos implements IListaArticulos {
+	
 
 	Vector<Articulo> articulos = new Vector();
-	Articulo articulo = new Articulo();
+	File archivo=new File("articulo.txt");
+	AccesoFichero accesoFichero;
+	Object articulo=new Object();
+	
+	/**
+	 * constructor
+	 * 
+	 * Crea una nueva instancia de AccesoFichero y rellena el vector
+	 * de objetos al leerlos del archivo.
+	 */
+	
+	public ListaArticulos() {
+		try {
+			accesoFichero=new AccesoFichero(archivo);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		articulo=accesoFichero.leerObjeto();
+		
+		while (articulo!=null) {
+			
+			articulos.add((Articulo) articulo);
+			articulo=accesoFichero.leerObjeto();
+			
+		}
+		
+	}
 
 	@Override
 	public Articulo buscarArticulo(String nombre) {
 		
 		for (int i = 0; i < articulos.size(); i++) {
-			if (nombre = articulos.get(i).getNombre()) {
-				return articulo;
+			if (nombre == articulos.get(i).getNombre()) {
+				return articulos.get(i);
 			}
 		}
 
@@ -24,8 +56,11 @@ public class ListaArticulos implements IListaArticulos {
 	@Override
 	public boolean insertarArticulo(Articulo articulo) {
 		
-		if(buscarArticulo(articulo.getNombre)!=null){
+		if(buscarArticulo(articulo.getNombre())==null){
+			
 			articulos.add(articulo);
+			accesoFichero.escribirObjeto(articulo);
+			
 			return true;
 		}
 		
