7a4a37fe5a9734015e6fdd7fecbec2b3ac8d80bb
Miguel <Miguel@Equipo16-PC>
2015-05-14 12:19:17 +0200

    Creada ListaPedidos en controlador

---
+++ Fruteria/src/controlador/ListaPedidos.java
@@ -0,0 +1,41 @@
+package controlador;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import modelo.AccesoFichero;
+import modelo.Pedido;
+
+public class ListaPedidos implements IListaPedidos {
+	File archivo = new File("ListaPedidos.txt");
+	AccesoFichero accesoFichero;
+	Pedido pedido;
+
+	@Override
+	public Pedido buscarPedido(int numero) {
+		try {
+			accesoFichero = new AccesoFichero(archivo);
+			pedido = (Pedido) accesoFichero.leerObjeto();
+			while (pedido != null) {
+				if (pedido.getNumeroPedido() == numero)
+					return pedido;
+				else{
+					pedido=(Pedido) accesoFichero.leerObjeto();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean altaPedido(Pedido pedido) {
+
+		if (buscarPedido(pedido.getNumeroPedido()) == null) {
+			accesoFichero.escribirObjeto(pedido);
+			return true;
+		} else
+			return false;
+	}
+
+}
