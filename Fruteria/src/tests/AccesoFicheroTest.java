0a151adce2c5730a822b3ee12709fe1fae531cfb
miguel <mhidalgoc91@gmail.com>
2015-05-18 18:38:56 +0200

    creacion de documento

---
+++ Fruteria/src/tests/AccesoFicheroTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccesoFicheroTest {
+
+	@Test
+	public void testAccesoFichero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLeerObjeto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEscribirObjeto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModificarObjeto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+}
