88be73b0496dfa725bde1fd4ffca681afbe891c2
marco <marcofernandezheras@gmail.com>
2015-05-19 09:45:57 +0200

    Implementacion de busqueda

--- Fruteria/src/controlador/ParaModCliente.java
+++ Fruteria/src/controlador/ParaModCliente.java
@@ -2,10 +2,16 @@ package controlador;
 
 import vista.ModClienteUI;
 import java.awt.event.ActionListener;
+
+import modelo.Cliente;
+
 import java.awt.event.ActionEvent;
 
 public class ParaModCliente extends ModClienteUI {
-	public ParaModCliente() {
+	
+	private IListaCliente listaCliente;
+	public ParaModCliente(IListaCliente listaCliente) {
+		this.listaCliente = listaCliente;
 		btnModificar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				buscarCliente();
@@ -21,7 +27,24 @@ public class ParaModCliente extends ModClienteUI {
 	}
 
 	private void buscarCliente() {
-		// TODO Auto-generated method stub
+		if(!txtBuscarNombre.getText().isEmpty() && !txtBuscarApellido.getText().isEmpty())
+		{
+			Cliente cliente = listaCliente.buscarCliente(txtBuscarNombre.getText(), txtBuscarApellido.getText());
+			if(cliente != null){
+				txtDNI.setText(cliente.getDni());
+				txtNombre.setText(cliente.getNombre());
+				txtApellidos.setText(cliente.getApellidos());
+				txtColorDePelo.setText(cliente.getColorPelo().toString());
+			}
+			else
+			{
+				txtMensaje.setText("No existe un cliente con ese nmbre y apellidos");
+			}
+		}
+		else
+		{
+			txtMensaje.setText("Debe introducir datos para poder buscar");
+		}
 		
 	}
 	private void modificarCliente() {
