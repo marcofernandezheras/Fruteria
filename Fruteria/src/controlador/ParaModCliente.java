a4bba4d85ee10b3e90e990108dfe4183ae71859d
marco <marcofernandezheras@gmail.com>
2015-05-19 10:09:49 +0200

    fin ParaModCliente

--- Fruteria/src/controlador/ParaModCliente.java
+++ Fruteria/src/controlador/ParaModCliente.java
@@ -3,13 +3,18 @@ package controlador;
 import vista.ModClienteUI;
 import java.awt.event.ActionListener;
 
+import com.aeat.valida.Validador;
+
 import modelo.Cliente;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 
 public class ParaModCliente extends ModClienteUI {
 	
 	private IListaCliente listaCliente;
+	private Object identificador;
+	private Cliente cliente;
 	public ParaModCliente(IListaCliente listaCliente) {
 		this.listaCliente = listaCliente;
 		btnModificar.addActionListener(new ActionListener() {
@@ -29,8 +34,9 @@ public class ParaModCliente extends ModClienteUI {
 	private void buscarCliente() {
 		if(!txtBuscarNombre.getText().isEmpty() && !txtBuscarApellido.getText().isEmpty())
 		{
-			Cliente cliente = listaCliente.buscarCliente(txtBuscarNombre.getText(), txtBuscarApellido.getText());
+			cliente = listaCliente.buscarCliente(txtBuscarNombre.getText(), txtBuscarApellido.getText());
 			if(cliente != null){
+				identificador = cliente.identificador();
 				txtDNI.setText(cliente.getDni());
 				txtNombre.setText(cliente.getNombre());
 				txtApellidos.setText(cliente.getApellidos());
@@ -48,7 +54,44 @@ public class ParaModCliente extends ModClienteUI {
 		
 	}
 	private void modificarCliente() {
-		// TODO Auto-generated method stub
-		
+		if(cliente != null) /* Si tenemos un cliente */
+		{ 
+			if(!txtNombre.getText().isEmpty() 
+					&& !txtApellidos.getText().isEmpty()
+					&& !txtDNI.getText().isEmpty()
+					&& !txtColorDePelo.getText().isEmpty() ) /* No hay ningún valor vacio */
+			{
+				Validador validador = new Validador();
+		        int e = validador.checkNif(txtDNI.getText());
+		 
+		        if (e > 0)
+		        {
+		        	cliente.setDni(txtDNI.getText());
+		        	cliente.setNombre(txtNombre.getText());
+		        	cliente.setApellidos(txtApellidos.getText());
+		        	cliente.setColorPelo(Color.getColor(txtColorDePelo.getText()));
+		        	if(listaCliente.modificarCliente(cliente, identificador))
+		        	{
+		        		identificador = cliente.identificador();
+		        	}
+		        	else
+		        	{
+		        		txtMensaje.setText("Error interno al modificar");
+		        	}
+		        }
+		        else
+				{
+					txtMensaje.setText("DNI erroneo");
+				}
+			}
+			else
+			{
+				txtMensaje.setText("Todos los campos son obligatorios");
+			}
+		}
+		else
+		{
+			txtMensaje.setText("Debe buscar un cliente a modificar");
+		}
 	}	
 }
