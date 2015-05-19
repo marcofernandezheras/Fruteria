ef9a02477349c386ac7ab55632223d2d64f722a6
marco <marcofernandezheras@gmail.com>
2015-05-19 09:39:11 +0200

    Inicio eventos

--- Fruteria/src/controlador/ParaModCliente.java
+++ Fruteria/src/controlador/ParaModCliente.java
@@ -1,7 +1,31 @@
 package controlador;
 
 import vista.ModClienteUI;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ParaModCliente extends ModClienteUI {
+	public ParaModCliente() {
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buscarCliente();
+			}
+		});
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				modificarCliente();
+			}
+			
+		});
+	}
 
+	private void buscarCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void modificarCliente() {
+		// TODO Auto-generated method stub
+		
+	}	
 }
