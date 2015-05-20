da33835ca95c60858c97ad6ca6148ee763d0bdbb
Danvgon <danvgon@gmail.com>
2015-05-20 09:59:35 +0200

    Redimensionadas 3 UI

--- Fruteria/src/vista/AltaArticuloUI.java
+++ Fruteria/src/vista/AltaArticuloUI.java
@@ -32,9 +32,9 @@ public class AltaArticuloUI extends JPanel {
 	 */
 	public AltaArticuloUI() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 78, 0, 59, 0, 0, 0, 26, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[] {30, 40, 30, 78, 30, 59, 30, 30, 30, 30};
+		gridBagLayout.rowHeights = new int[] {30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
 		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
