90113bc097e6a1a444f764683d86767ce4ec603f
Danvgon <danvgon@gmail.com>
2015-05-21 10:58:00 +0200

    cambios de margenes y alineacion en AltaClienteUI

--- Fruteria/src/vista/AltaClienteUI.java
+++ Fruteria/src/vista/AltaClienteUI.java
@@ -27,14 +27,14 @@ public class AltaClienteUI extends JPanel {
 	 */
 	public AltaClienteUI() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {30, 10, 250, 30, 2, 30};
+		gridBagLayout.columnWidths = new int[] {30, 10, 250, 30, 2, 30, 0};
 		gridBagLayout.rowHeights = new int[] {30, 19, 20, 30, 10, 18};
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, 0.0 };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		setLayout(gridBagLayout);
 		
 		GridBagConstraints gbc_lblAltaClientes = new GridBagConstraints();
-		gbc_lblAltaClientes.gridheight = 2;
+		gbc_lblAltaClientes.gridwidth = 2;
 		gbc_lblAltaClientes.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAltaClientes.gridx = 2;
 		gbc_lblAltaClientes.gridy = 0;
@@ -45,78 +45,81 @@ public class AltaClienteUI extends JPanel {
 												gbc_lblNombre.anchor = GridBagConstraints.EAST;
 												gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
 												gbc_lblNombre.gridx = 1;
-												gbc_lblNombre.gridy = 2;
+												gbc_lblNombre.gridy = 1;
 												add(lblNombre, gbc_lblNombre);
 								
 										txtNombre = new JTextField();
 										GridBagConstraints gbc_txtNombre = new GridBagConstraints();
-										gbc_txtNombre.gridwidth = 2;
+										gbc_txtNombre.gridwidth = 4;
 										gbc_txtNombre.insets = new Insets(0, 0, 5, 5);
 										gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
 										gbc_txtNombre.gridx = 2;
-										gbc_txtNombre.gridy = 2;
+										gbc_txtNombre.gridy = 1;
 										add(txtNombre, gbc_txtNombre);
 										txtNombre.setColumns(10);
+										
+												GridBagConstraints gbc_lblApellidos = new GridBagConstraints();
+												gbc_lblApellidos.anchor = GridBagConstraints.EAST;
+												gbc_lblApellidos.insets = new Insets(0, 0, 5, 5);
+												gbc_lblApellidos.gridx = 1;
+												gbc_lblApellidos.gridy = 3;
+												add(lblApellidos, gbc_lblApellidos);
+								
+										txtApellidos = new JTextField();
+										GridBagConstraints gbc_txtApellidos = new GridBagConstraints();
+										gbc_txtApellidos.gridwidth = 4;
+										gbc_txtApellidos.insets = new Insets(0, 0, 5, 5);
+										gbc_txtApellidos.fill = GridBagConstraints.HORIZONTAL;
+										gbc_txtApellidos.gridx = 2;
+										gbc_txtApellidos.gridy = 3;
+										add(txtApellidos, gbc_txtApellidos);
+										txtApellidos.setColumns(10);
 						
-								GridBagConstraints gbc_lblApellidos = new GridBagConstraints();
-								gbc_lblApellidos.anchor = GridBagConstraints.EAST;
-								gbc_lblApellidos.insets = new Insets(0, 0, 5, 5);
-								gbc_lblApellidos.gridx = 1;
-								gbc_lblApellidos.gridy = 4;
-								add(lblApellidos, gbc_lblApellidos);
+								GridBagConstraints gbc_lblDni = new GridBagConstraints();
+								gbc_lblDni.anchor = GridBagConstraints.EAST;
+								gbc_lblDni.insets = new Insets(0, 0, 5, 5);
+								gbc_lblDni.gridx = 1;
+								gbc_lblDni.gridy = 5;
+								add(lblDni, gbc_lblDni);
+						
+								txtDNI = new JTextField();
+								GridBagConstraints gbc_txtDNI = new GridBagConstraints();
+								gbc_txtDNI.gridwidth = 4;
+								gbc_txtDNI.insets = new Insets(0, 0, 5, 5);
+								gbc_txtDNI.fill = GridBagConstraints.HORIZONTAL;
+								gbc_txtDNI.gridx = 2;
+								gbc_txtDNI.gridy = 5;
+								add(txtDNI, gbc_txtDNI);
+								txtDNI.setColumns(10);
 				
-						txtApellidos = new JTextField();
-						GridBagConstraints gbc_txtApellidos = new GridBagConstraints();
-						gbc_txtApellidos.gridwidth = 2;
-						gbc_txtApellidos.insets = new Insets(0, 0, 5, 5);
-						gbc_txtApellidos.fill = GridBagConstraints.HORIZONTAL;
-						gbc_txtApellidos.gridx = 2;
-						gbc_txtApellidos.gridy = 4;
-						add(txtApellidos, gbc_txtApellidos);
-						txtApellidos.setColumns(10);
-		
-				GridBagConstraints gbc_lblDni = new GridBagConstraints();
-				gbc_lblDni.anchor = GridBagConstraints.EAST;
-				gbc_lblDni.insets = new Insets(0, 0, 5, 5);
-				gbc_lblDni.gridx = 1;
-				gbc_lblDni.gridy = 6;
-				add(lblDni, gbc_lblDni);
-				
-						txtDNI = new JTextField();
-						GridBagConstraints gbc_txtDNI = new GridBagConstraints();
-						gbc_txtDNI.gridwidth = 2;
-						gbc_txtDNI.insets = new Insets(0, 0, 5, 5);
-						gbc_txtDNI.fill = GridBagConstraints.HORIZONTAL;
-						gbc_txtDNI.gridx = 2;
-						gbc_txtDNI.gridy = 6;
-						add(txtDNI, gbc_txtDNI);
-						txtDNI.setColumns(10);
-		
-				GridBagConstraints gbc_lblColorDePelo = new GridBagConstraints();
-				gbc_lblColorDePelo.anchor = GridBagConstraints.EAST;
-				gbc_lblColorDePelo.insets = new Insets(0, 0, 5, 5);
-				gbc_lblColorDePelo.gridx = 1;
-				gbc_lblColorDePelo.gridy = 8;
-				add(lblColorDePelo, gbc_lblColorDePelo);
+						GridBagConstraints gbc_lblColorDePelo = new GridBagConstraints();
+						gbc_lblColorDePelo.gridwidth = 2;
+						gbc_lblColorDePelo.anchor = GridBagConstraints.EAST;
+						gbc_lblColorDePelo.insets = new Insets(0, 0, 5, 5);
+						gbc_lblColorDePelo.gridx = 0;
+						gbc_lblColorDePelo.gridy = 7;
+						add(lblColorDePelo, gbc_lblColorDePelo);
 		
 				txtColorDePelo = new JTextField();
 				GridBagConstraints gbc_txtColorDePelo = new GridBagConstraints();
-				gbc_txtColorDePelo.gridwidth = 2;
+				gbc_txtColorDePelo.gridwidth = 4;
 				gbc_txtColorDePelo.insets = new Insets(0, 0, 5, 5);
 				gbc_txtColorDePelo.fill = GridBagConstraints.HORIZONTAL;
 				gbc_txtColorDePelo.gridx = 2;
-				gbc_txtColorDePelo.gridy = 8;
+				gbc_txtColorDePelo.gridy = 7;
 				add(txtColorDePelo, gbc_txtColorDePelo);
 				txtColorDePelo.setColumns(10);
-
-		GridBagConstraints gbc_btnCrear = new GridBagConstraints();
-		gbc_btnCrear.gridheight = 2;
-		gbc_btnCrear.gridwidth = 2;
-		gbc_btnCrear.insets = new Insets(0, 0, 0, 5);
-		gbc_btnCrear.gridx = 3;
-		gbc_btnCrear.gridy = 9;
-		btnCrear.setIcon(new ImageIcon(AltaClienteUI.class.getResource("/iconos/plus.png")));
-		add(btnCrear, gbc_btnCrear);
+		
+				GridBagConstraints gbc_btnCrear = new GridBagConstraints();
+				gbc_btnCrear.anchor = GridBagConstraints.WEST;
+				gbc_btnCrear.fill = GridBagConstraints.VERTICAL;
+				gbc_btnCrear.insets = new Insets(0, 0, 0, 5);
+				gbc_btnCrear.gridheight = 2;
+				gbc_btnCrear.gridwidth = 3;
+				gbc_btnCrear.gridx = 3;
+				gbc_btnCrear.gridy = 8;
+				btnCrear.setIcon(new ImageIcon(AltaClienteUI.class.getResource("/iconos/plus.png")));
+				add(btnCrear, gbc_btnCrear);
 
 	}
 }
